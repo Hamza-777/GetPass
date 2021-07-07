93487b8c343b00626a607b1d057528be923e97d0
Hamza-777 <hamzararrani77@gmail.com>
2021-07-07 16:58:55 +0530

    finished

---
+++ index.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+const program = require('commander');
+const chalk = require('chalk');
+const clipboardy = require('clipboardy');
+const log = console.log;
+const createPassword = require('./utils/createPassword');
+const savePassword = require('./utils/savePassword');
+
+program.version('1.0.0').description('A password generator')
+
+program
+    .option('-l, --length <length>', 'length of password', '7')
+    .option('-s, --save', 'save the password to passwords.txt')
+    .option('-nn, --no-numbers', 'exclude numbers')
+    .option('-ns, --no-symbols', 'exclude symbols')
+    .parse();
+
+const {length, save, numbers, symbols} = program.opts();
+
+// get generated password
+const generatedPassword = createPassword(length, numbers, symbols);
+
+// save to file
+if(save) {
+    savePassword(generatedPassword);
+}
+
+// copy to clipboard
+clipboardy.writeSync(generatedPassword);
+
+// output generated password
+log(chalk.red('Generated Password: ') + chalk.bold(generatedPassword));
+log(chalk.blue('Password copied to clipboard'));
